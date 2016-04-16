ad330217a99e847d4a719bac7ae9f033550f36ab
CoderBryGuy <bryan.dov.bergman@gmail.com>
2016-04-16 19:14:54 +0300

    All homework questions 1-5 except for 3 which I didn't understand

--- app/src/main/java/com/example/hackeru/exercises_april13/MainActivity.java
+++ app/src/main/java/com/example/hackeru/exercises_april13/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.hackeru.exercises_april13;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+    public static final String USER_1 = "USER_1";
+    public static final String INT_1 = "INT_1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +17,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    void btnOnClick(){
+    void btnOnClick(View view){
         Intent intent = new Intent(this, Sec_Activity.class);
+        Bundle extras = new Bundle();
+        extras.putString(USER_1,"Bryan's App");
+        extras.putInt(INT_1, 25);
+        intent.putExtras(extras);
         startActivity(intent);
     }
 }
